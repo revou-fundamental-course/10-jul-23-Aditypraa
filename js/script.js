a83da352895810e86ecebc0eb92a8006df89d7f6
Aditya Pratama <aditypraa@gmail.com>
2023-07-15 15:16:37 +0700

    Create Variabel in file script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let age = document.getElementById("age");
+let height = document.getElementById("height");
+let weight = document.getElementById("weight");
+let male = document.getElementById("male");
+let female = document.getElementById("female");
+let form = document.getElementById("form");
+let modal = document.getElementById("myModal");
+
+let resultArea = document.querySelector(".comment");
+modalContent = document.querySelector(".modal-content");
+modalText = document.querySelector("#modalText");
+
+let span = document.getElementsByClassName("close")[0];
