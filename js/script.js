d69bdfd130bc066643a57a13cf991f70fe23982f
Aditya Pratama <aditypraa@gmail.com>
2023-07-16 02:48:28 +0700

    trial fitur

--- js/script.js
+++ js/script.js
@@ -67,6 +67,13 @@ function countBmi() {
   document.querySelector("#result").innerHTML = bmi.toFixed(2);
 }
 
+// Function click close
 span.onclick = function () {
   modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 };
